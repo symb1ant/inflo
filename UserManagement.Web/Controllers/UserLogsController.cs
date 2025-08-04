0f19739f39d2876477e1c62ed6bfdd2d9564d9f6
john dickson <isamu.alva@outlook.com>
2025-08-04 20:15:29 +0100

    Add logs page and views

---
+++ UserManagement.Web/Controllers/UserLogsController.cs
@@ -0,0 +1,65 @@
+﻿using System.Linq;
+using UserManagement.Services.Domain.Interfaces;
+using UserManagement.Web.Models.UserLogs;
+using static System.Math;
+
+namespace UserManagement.WebMS.Controllers;
+
+public class UserLogsController : Controller
+{
+    private readonly IUserLogService _userLogService;
+
+    public UserLogsController(IUserLogService userLogService)
+    {
+        _userLogService = userLogService;
+    }
+
+    [HttpGet("list/{page?}")]
+    public ViewResult List(int page = 1)
+    {
+        const int pageSize = 10;
+        var logs = _userLogService.GetAllLogs()
+            .Skip((page - 1) * pageSize)
+            .Take(pageSize);
+
+        var viewModel = new UserLogListVIewModel
+        {
+            Items = logs.Select(log => new UserLogListVIewModel.UserLogListItemViewModel
+            {
+                Id = log.Id,
+                UserId = log.UserId,
+                FieldName = log.FieldName,
+                OldValue = log.OldValue,
+                NewValue = log.NewValue,
+                ChangedAt = log.ChangedAt
+            }).ToList()
+        };
+
+        viewModel.PageCount = (int)Ceiling((double)_userLogService.GetAllLogs().Count() / pageSize);
+        viewModel.CurrentPage = page;
+
+        return View(viewModel);
+    }
+
+    [HttpGet("details/{id}")]
+    public ViewResult Details(long id)
+    {
+        var log = _userLogService.GetLogsByUserId(id).FirstOrDefault();
+        if (log == null)
+        {
+            return View("NotFound");
+        }
+
+        var viewModel = new UserLogListVIewModel.UserLogListItemViewModel
+        {
+            Id = log.Id,
+            UserId = log.UserId,
+            FieldName = log.FieldName,
+            OldValue = log.OldValue,
+            NewValue = log.NewValue,
+            ChangedAt = log.ChangedAt
+        };
+
+        return View(viewModel);
+    }
+}
---
+++ UserManagement.Web/Models/UserLogs/UserLogListVIewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace UserManagement.Web.Models.UserLogs;
+
+public class UserLogListVIewModel
+{
+    public List<UserLogListItemViewModel> Items { get; set; } = new();
+
+    public class UserLogListItemViewModel
+    {
+        public long Id { get; set; }
+        public long UserId { get; set; }
+        public string? FieldName { get; set; }
+        public string? OldValue { get; set; }
+        public string? NewValue { get; set; }
+        public DateTime ChangedAt { get; set; }
+    }
+
+    public int PageCount { get; set; } = 0;
+    public int CurrentPage { get; set; } = 1;
+}
